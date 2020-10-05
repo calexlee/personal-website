693c03f5d0a3e65894dcee2a2865a581e03da080
Alex Lee <cal368@cornell.edu>
2020-10-05 00:14:29 -0400

    Added images and photos to HomePage
    Added animate.css animations as well

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,21 @@ import './Content.css';
 const Home = () => {
     return (
         <div>
+            <div className = "nameTitle">
             <h2 className = "homeTitle">Clifford <span className = "nick">Alex</span>ander Lee</h2>
             <hr className="hTitle"/>
-            <h2 className = "CS">Computer Science Student @ <span className = "Cornell">Cornell</span></h2>
-            <h2 className = "SA"><span className = "MATLAB">MATLAB</span> Student Ambassador</h2>
+            <img className = "animated hidSun" src = {require('./content/SunGlasses.png')} alt = "Sunglasses" height = "100"/>
+            </div>
+
+            <div className = "Cornell Line">
+            <h2 className = "CS">Computer Science Student @ <span className = "animated Cornell">Cornell</span></h2>
+            <img className = "animated hidCorn" src = {require('./content/cornell_seal_simple_black.svg')} alt = "Cornell Logo" height = "100"/>
+            </div>
+
+            <div className = "MathWorksLine">
+            <h2 className = "SA"><span className = "animated MATLAB">MATLAB</span> Student Ambassador</h2>
+            <img className = "animated hidMAT" src = {require('./content/L-Membrane_RGB_R.png')} alt = "MathWorks L-shaped Membrane" height = "100"/>
+            </div>
         </div>
     )
 };
